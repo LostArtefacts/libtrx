ae698ce67b8dc5fb84ce2b01e67a40ac6c787aa9
Marcin Kurczewski <dash@wind.garden>
2024-04-29 20:46:58 +0200

    strings: add from TR1X

--- include/libtrx/strings.h
+++ include/libtrx/strings.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
 
 bool String_EndsWith(const char *str, const char *suffix);
 bool String_Equivalent(const char *a, const char *b);
+
+const char *String_CaseSubstring(const char *subject, const char *pattern);
+int32_t String_Match(const char *subject, const char *pattern);
