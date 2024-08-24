d655b88d1ee9d415569d7f8a328f883f47e1add8
Marcin Kurczewski <dash@wind.garden>
2024-08-24 23:22:49 +0200

    config: move option dumping and loading from TR1X

--- include/libtrx/config/config_file.h
+++ include/libtrx/config/config_file.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "../enum_str.h"
 #include "../json.h"
+#include "config_option.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -9,3 +11,15 @@ bool ConfigFile_Read(
     const char *path, void (*load)(struct json_object_s *root_obj));
 bool ConfigFile_Write(
     const char *path, void (*dump)(struct json_object_s *root_obj));
+
+void ConfigFile_LoadOptions(
+    struct json_object_s *root_obj, const CONFIG_OPTION *options);
+void ConfigFile_DumpOptions(
+    struct json_object_s *root_obj, const CONFIG_OPTION *options);
+
+int ConfigFile_ReadEnum(
+    struct json_object_s *obj, const char *name, int default_value,
+    const ENUM_STRING_MAP *enum_map);
+void ConfigFile_WriteEnum(
+    struct json_object_s *obj, const char *name, int value,
+    const ENUM_STRING_MAP *enum_map);
--- src/config/config_file.c
+++ src/config/config_file.c
@@ -10,6 +10,8 @@ static bool Config_ReadFromJSON(
     const char *json, void (*load)(struct json_object_s *root_obj));
 static char *Config_WriteToJSON(void (*dump)(struct json_object_s *root_obj));
 
+static const char *ConfigFile_ResolveOptionName(const char *option_name);
+
 static bool Config_ReadFromJSON(
     const char *cfg_data, void (*load)(struct json_object_s *root_obj))
 {
@@ -54,6 +56,15 @@ static char *Config_WriteToJSON(void (*dump)(struct json_object_s *root_obj))
     return data;
 }
 
+static const char *ConfigFile_ResolveOptionName(const char *option_name)
+{
+    const char *dot = strrchr(option_name, '.');
+    if (dot) {
+        return dot + 1;
+    }
+    return option_name;
+}
+
 bool ConfigFile_Read(
     const char *path, void (*load)(struct json_object_s *root_obj))
 {
@@ -87,4 +98,113 @@ bool ConfigFile_Write(
     Memory_FreePointer(&data);
 
     return true;
+}
+
+void ConfigFile_LoadOptions(
+    struct json_object_s *root_obj, const CONFIG_OPTION *options)
+{
+    const CONFIG_OPTION *opt = options;
+    while (opt->target) {
+        switch (opt->type) {
+        case COT_BOOL:
+            *(bool *)opt->target = json_object_get_bool(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(bool *)opt->default_value);
+            break;
+
+        case COT_INT32:
+            *(int32_t *)opt->target = json_object_get_int(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(int32_t *)opt->default_value);
+            break;
+
+        case COT_FLOAT:
+            *(float *)opt->target = json_object_get_double(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(float *)opt->default_value);
+            break;
+
+        case COT_DOUBLE:
+            *(double *)opt->target = json_object_get_double(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(double *)opt->default_value);
+            break;
+
+        case COT_ENUM:
+            *(int *)opt->target = ConfigFile_ReadEnum(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(int *)opt->default_value,
+                (const ENUM_STRING_MAP *)opt->param);
+        }
+        opt++;
+    }
+}
+
+void ConfigFile_DumpOptions(
+    struct json_object_s *root_obj, const CONFIG_OPTION *options)
+{
+    const CONFIG_OPTION *opt = options;
+    while (opt->target) {
+        switch (opt->type) {
+        case COT_BOOL:
+            json_object_append_bool(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(bool *)opt->target);
+            break;
+
+        case COT_INT32:
+            json_object_append_int(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(int32_t *)opt->target);
+            break;
+
+        case COT_FLOAT:
+            json_object_append_double(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(float *)opt->target);
+            break;
+
+        case COT_DOUBLE:
+            json_object_append_double(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(double *)opt->target);
+            break;
+
+        case COT_ENUM:
+            ConfigFile_WriteEnum(
+                root_obj, ConfigFile_ResolveOptionName(opt->name),
+                *(int *)opt->target, (const ENUM_STRING_MAP *)opt->param);
+            break;
+        }
+        opt++;
+    }
+}
+
+int ConfigFile_ReadEnum(
+    struct json_object_s *obj, const char *name, int default_value,
+    const ENUM_STRING_MAP *enum_map)
+{
+    const char *value_str = json_object_get_string(obj, name, NULL);
+    if (value_str) {
+        while (enum_map->text) {
+            if (!strcmp(value_str, enum_map->text)) {
+                return enum_map->value;
+            }
+            enum_map++;
+        }
+    }
+    return default_value;
+}
+
+void ConfigFile_WriteEnum(
+    struct json_object_s *obj, const char *name, int value,
+    const ENUM_STRING_MAP *enum_map)
+{
+    while (enum_map->text) {
+        if (enum_map->value == value) {
+            json_object_append_string(obj, name, enum_map->text);
+            break;
+        }
+        enum_map++;
+    }
 }
