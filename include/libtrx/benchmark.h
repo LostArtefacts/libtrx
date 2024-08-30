1b81d18a83899a6694608409b25913e3376662db
Marcin Kurczewski <dash@wind.garden>
2024-08-31 00:47:38 +0200

    benchmark: support incremental benchmarking

--- include/libtrx/benchmark.h
+++ include/libtrx/benchmark.h
@@ -4,13 +4,21 @@
 
 typedef struct {
     Uint64 start;
+    Uint64 last;
 } BENCHMARK;
 
 BENCHMARK *Benchmark_Start(void);
 
-#define Benchmark_End(...)                                                     \
-    Benchmark_End_Impl(__FILE__, __LINE__, __func__, __VA_ARGS__)
+#define Benchmark_End(b, ...)                                                  \
+    Benchmark_End_Impl(b, __FILE__, __LINE__, __func__, __VA_ARGS__)
+
+#define Benchmark_Tick(b, ...)                                                 \
+    Benchmark_Tick_Impl(b, __FILE__, __LINE__, __func__, __VA_ARGS__)
 
 void Benchmark_End_Impl(
-    const char *file, int32_t line, const char *func, BENCHMARK *b,
+    BENCHMARK *b, const char *file, int32_t line, const char *func,
+    const char *message);
+
+void Benchmark_Tick_Impl(
+    BENCHMARK *b, const char *file, int32_t line, const char *func,
     const char *message);
