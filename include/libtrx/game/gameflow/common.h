f40b311164375db0b85d048d2c0381f2c63563cf
Marcin Kurczewski <dash@wind.garden>
2024-09-30 12:59:28 +0200

    console/cmd: import /play

--- include/libtrx/game/gameflow/common.h
+++ include/libtrx/game/gameflow/common.h
@@ -2,4 +2,9 @@
 
 #include "./types.h"
 
+extern int32_t Gameflow_GetLevelCount(void);
+extern const char *Gameflow_GetLevelFileName(int32_t level_num);
+extern const char *Gameflow_GetLevelTitle(int32_t level_num);
+extern int32_t Gameflow_GetGymLevelNumber(void);
+
 extern void Gameflow_OverrideCommand(GAMEFLOW_COMMAND action);
